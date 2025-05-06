05bd0b7023c878560971ecb26ed47635f0ac04f7
Nayan Lakhwani <nayan@Nayans-Mac-mini.local>
2025-05-06 20:49:57 +0530

    feat: make context service async to handle concurrent load[]

---
+++ src/modules/app/middleware/context.middleware.ts
@@ -0,0 +1,14 @@
+import { Injectable, NestMiddleware } from "@nestjs/common";
+import { ContextService } from "@src/modules/common/services/context.service";
+
+@Injectable()
+export class ContextMiddleware implements NestMiddleware {
+  constructor(private readonly contextService: ContextService) {}
+
+  use(req: Request, res: Response, next: any) {
+    this.contextService.run(() => {
+      // Optionally set initial context data here
+      next();
+    });
+  }
+}
--- src/modules/common/services/context.service.ts
+++ src/modules/common/services/context.service.ts
@@ -1,14 +1,25 @@
 import { Injectable } from "@nestjs/common";
+import { AsyncLocalStorage } from "async_hooks";
 
 @Injectable()
 export class ContextService {
-  private contextData: Record<string, any> = {};
+  private readonly asyncLocalStorage = new AsyncLocalStorage<
+    Map<string, any>
+  >();
 
   set(key: string, value: any) {
-    this.contextData[key] = value;
+    const store = this.asyncLocalStorage.getStore();
+    if (store) {
+      store.set(key, value);
+    }
   }
 
   get(key: string) {
-    return this.contextData[key];
+    const store = this.asyncLocalStorage.getStore();
+    return store ? store.get(key) : undefined;
+  }
+
+  run(callback: () => void) {
+    this.asyncLocalStorage.run(new Map<string, any>(), callback);
   }
 }
